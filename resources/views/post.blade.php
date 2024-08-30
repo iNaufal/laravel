a968c221f1f04d7679b75f1a976935536eaa695f
Veno veno <naufalpriatna26@gmail.com>
2024-08-31 00:17:47 +0700

    add: Eloquent Relationship

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,7 +4,7 @@
   <article class="py-8 max-w-screen-md">
     <div class="text-base text-gray-500">
       <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900 text-justify">{{ $post['title'] }}</h2>
-      <a href="#">{{ $post['author'] }}</a> | {{ $post->created_at->diffForHumans() }}
+      <a href="/authors/{{ $post->author->id }}">{{ $post->author->name }}</a> | {{ $post->created_at->diffForHumans() }}
     </div>
     <p class="my-4 font-light text-justify">{{ $post['body'] }}</p>
     <a href="/posts" class="font-medium text-blue-500">&laquo; Back to Posts</a>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,11 +3,11 @@
 
   @foreach ($posts as $post)
     <article class="py-8 max-w-screen-md border-b border-gray-300">
+      <a href="/posts/{{ $post['slug'] }}" class="hover:underline">
+        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900 text-justify">{{ $post['title'] }}</h2>
+      </a>
       <div class="text-base text-gray-500">
-        <a href="/posts/{{ $post['slug'] }}" class="hover:underline">
-          <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900 text-justify">{{ $post['title'] }}</h2>
-        </a>
-        <a href="#">{{ $post['author'] }}</a> | {{ $post->created_at->diffForHumans() }}
+        <a href="/authors/{{ $post->author->id }}" class="hover:text-blue-500 transition duration-300">{{ $post->author->name }}</a> | {{ $post->created_at->diffForHumans() }}
       </div>
       <p class="my-4 font-light text-justify">{{ Str::limit($post['body'], 200) }}</p>
       <a href="/posts/{{ $post['slug'] }}" class="font-medium text-blue-500">Read more &raquo;</a>
